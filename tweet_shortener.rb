8ea4309fe7088e544176bc5240d40748089814aa
eselbeus <eselbeus@gmail.com>
2019-01-12 00:56:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ end
 def word_substituter(tweet)
   tweet_arr = tweet.split(" ")
   revised = tweet_arr.map do |word|
-    if dictionary.keys.include?(word) || dictionary.keys.include?(word.downcase)
+    if dictionary.keys.include?(word) || dictionary.keys.capitalized.include?(word)
       dictionary[word]
     else
       word
